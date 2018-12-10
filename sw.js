b2a7a836dc1252852073430fa82a2c7a6c4a98d5
26314fdg <ferry.degroot@eur.nl>
2018-12-10 13:20:47 +0100

    Add serviceworker

--- sw.js
+++ sw.js
@@ -1,7 +1,41 @@
 importScripts('js/sw-toolbox.js');
 
+toolbox.options.debug = true;
+
 toolbox.precache(['/offline.html', '/svg/sad.svg']);
 
-toolbox.router.get(/.*\.[css|js]$/, toolbox.cacheFirst);
+// toolbox.router.get('/',
+//   function(request, values, options) {
+//     console.log('/ requested');
+//     event.respondWith(new Response('pipo'));
+//     // return toolbox.networkFirst(request, values, options)
+//     // .catch(function(err) {
+//     //   caches.match(new Request('offline.html'));
+//     // });
+//   },
+//   { cache: { name: 'html', maxAgeSeconds: 86400 /* cache for a day */ }}
+// );
 
-toolbox.router.get(/^index\.html$/, toolbox.networkFirst);
+
+
+
+
+
+toolbox.router.get('/*.css', toolbox.cacheFirst);
+
+toolbox.router.get('/*.js', toolbox.cacheFirst);
+
+toolbox.router.get('/', function(request, values, options) {
+  return toolbox.networkOnly(request, values, options)
+    .catch(function(err) {
+      caches.match(new Request('/offline.html'));
+    });
+});
+
+
+// toolbox.router.get('/', function(request, values, options) {
+//   console.log('.html requested');
+//   return toolbox.networkOnly(request, values, options);
+// });
+//
+// toolbox.router.default = toolbox.networkOnly;
